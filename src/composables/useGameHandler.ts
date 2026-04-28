02e9eb0d7460b7a5af668750f5b3f975076140b1
Minh Long Vu <minhlong.vu@outlook.de>
2026-04-28 16:38:54 +0200

    feature: `useGameHandler.ts`: added startGame function

--- src/composables/useGameHandler.ts
+++ src/composables/useGameHandler.ts
@@ -9,7 +9,7 @@ interface Player {
     legsWon: number,  //score-tracker for multiple legs
 }
 
-// declare the current state of the game
+// declare the current state of the game -> source of truth
 const state = reactive({
     phase: 'setup' as 'setup' | 'game' | 'winner',
     players: [] as Player[],
@@ -19,6 +19,7 @@ const state = reactive({
     startingPlayerIndex: 0,
 });
 
+// Derived state , takes raw data and devies it into only readable values --> only gets the results
 export function useGame() {
     const currentPlayer = computed(() => {
         return state.players[state.currentPlayerIndex]
@@ -31,4 +32,22 @@ export function useGame() {
     const matchWinner = computed(() => {
         return state.players.find(p => p.legsWon >= legsNeededToWin.value)
     })
+}
+
+function startGame(names: string[], legs: number) {
+  state.phase = 'game'
+  state.totalLegs = legs
+  state.currentLeg = 1
+  state.currentPlayerIndex = 0
+  state.startingPlayerIndex = 0
+
+//generates the whole player interface for every Name of the Name-Array
+  state.players = names.map(name => ({
+    name,
+    score: 501,
+    dartsThrown: 0,
+    totalPointsScored: 0,
+    legsWon: 0,
+    dartsThisLeg: 0,
+  }))
 }
