5619fa7ac4b5a87ca6d6894d076f8b47f364f265
Minh Long Vu <minhlong.vu@outlook.de>
2026-04-28 17:57:35 +0200

    Refactor: `useGamerHandler.ts`: cleaning up the types and moving it to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
+import { ref, reactive, computed} from "vue";
 
+export interface GameSettings {
+    pointsToWin: number,
+    mode: 'double-out' | 'single-out',
+}
+
+export interface Player {
+    name: string,
+    score: number,  //score left to shoot
+    dartsThisLeg: number,
+    totalPointsScored: number,  //not displayed, needed for average
+    legsWon: number,  //score-tracker for multiple legs
+}
+
+export type Session = {
+    players: Player[],
+    currentPlayerIndex: number,
+    totalLegs: number,
+    currentLeg: number,
+    startingPlayerIndex: number,
+    lastModified: Date,
+}
+
+const sessions = reactive<Session[]>([]);
 </script>
+
 <template>
     <div>
         Hello world!
