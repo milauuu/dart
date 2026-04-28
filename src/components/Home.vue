bc94a15ddca6fa31cf12d434c025b322f1e57963
Minh Long Vu <minhlong.vu@outlook.de>
2026-04-28 19:15:35 +0200

    feature: `Home.vue`: adding start game functions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { matches, type MatchSettings } from '../matches.ts';
+
+function startGame(matchSettings: MatchSettings, names: string[]) {
+    const today = new Date();
+    matches.push({
+        matchSettings,
+        players: names.map(name => ({
+            name,
+            score: matchSettings.pointsToWin,
+            dartsThisLeg: 0,
+            totalPointsScored: 0,
+        })),
+        currentPlayerIndex: 0,
+        startingPlayerIndex: 0,
+        lastModified: today,
+    });
+}
+</script>
 <template>
     start view
 </template>
