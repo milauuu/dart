970ce2632b2d4e02b7161aa4a019aacac5d09d99
Minh Long Vu <minhlong.vu@outlook.de>
2026-04-28 18:01:02 +0200

    fix: rename session to match

---
+++ src/matches.ts
@@ -0,0 +1,25 @@
+import { ref, reactive, computed} from "vue";
+
+export interface GameSettings {
+    pointsToWin: number,
+    mode: 'double-out' | 'single-out',
+}
+
+export interface Player {
+    name: string,
+    score: number,  //score left to shoot
+    dartsThisLeg: number,
+    totalPointsScored: number,  //not displayed, needed for average
+    legsWon: number,  //score-tracker for multiple legs
+}
+
+export type Match = {
+    players: Player[],
+    currentPlayerIndex: number,
+    totalLegs: number,
+    currentLeg: number,
+    startingPlayerIndex: number,
+    lastModified: Date,
+}
+
+export const sessions = reactive<Match[]>([]);
