7aa803bc8aa6f1f002e56dd009deb19cda5c72f8
Minh Long Vu <minhlong.vu@outlook.de>
2026-05-09 15:22:09 +0200

    refactor:`util.ts`: removing custom useEventListener() function

--- src/util.ts
+++ src/util.ts
@@ -1,48 +1,20 @@
 import { ref, type Ref, toValue, watchEffect, computed, watch } from 'vue';
+import { useEventListener } from "@vueuse/core";
 
 export function useFocus(targetElements: Ref<Array<Element | null>>) {
+    // reactive state (to be returned by this composable)
     const focusState = ref<boolean[]>([]);
-    useEventListener(targetElements, ['focusin', 'focusout'], () => {
-        focusState.value = targetElements.value.map((el) =>
-            el ? el.contains(document.activeElement) : false
-        );
+
+    const targetElementsFiltered = computed(() => targetElements.value.filter(el => el !== null));
+
+    useEventListener(targetElementsFiltered, ['focusin', 'focusout'], () => {
+        focusState.value = targetElements.value.map((el) => {
+            if (el === null) {
+                return false;
+            }
+            return el.contains(document.activeElement);
+        });
     });
+
     return focusState;
-}
-
-export function useEventListener(
-    targetElements: Ref<Array<Element | null>>,
-    eventNames: string[],
-    listener: (event: Event) => void
-) {
-    // the elements from the last iteration of `watchEffect()`
-    let oldElements = [] as Element[];
-
-    watchEffect(() => {
-        // add listeners to new elements
-        for (const newElement of toValue(targetElements) ?? []) {
-            if (!newElement || oldElements.includes(newElement)) {
-                continue;
-            }
-            // update `oldElements`
-            oldElements.push(newElement);
-            // add listener
-            for (const eventName of eventNames) {
-                newElement?.addEventListener(eventName, listener);
-            }
-        }
-
-        // remove listeners from elements that are not part of `targetElements` anymore
-        for (const oldElement of oldElements) {
-            if (toValue(targetElements)?.includes(oldElement)) {
-                continue;
-            }
-            // update `oldElements`
-            oldElements.splice(oldElements.indexOf(oldElement), 1);
-            // remove listener
-            for (const eventName of eventNames) {
-                oldElement.removeEventListener(eventName, listener);
-            }
-        }
-    });
 }
